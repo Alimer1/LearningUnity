eaa46b4a39527d99776edd0e0a3bea899bf4e290
Alimer1 <aliomermsrl@gmail.com>
2023-12-17 11:31:33 +0000

    3 week done. Added a custom special function as well.

--- Project4/Assets/Scripts/FunctionLibrary.cs
+++ Project4/Assets/Scripts/FunctionLibrary.cs
@@ -6,14 +6,41 @@ public static class FunctionLibrary
 
     public delegate Vector3 Function(float u, float v, float t);
 
-    public enum FunctionName                {Wave, MultiWave, Ripple, Sphere, Torus};
+    public enum FunctionName                {Wave, MultiWave, Ripple, Sphere, Torus, Special};
 
-    private static Function[] function =    {Wave, MultiWave, Ripple, Sphere, Torus};
+    private static Function[] function =    {Wave, MultiWave, Ripple, Sphere, Torus, Special};
 
     public static Function GetFunction (FunctionName name)
     {
         return function[(int)name];
     }
+    
+    public static FunctionName GetNextFunctionName(FunctionName name)
+    {
+        if((int)name < function.Length - 1)
+        {
+            return name + 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    public static FunctionName GetRandomFunctionName()
+    {
+        var choice = (FunctionName)Random.Range(0,function.Length);
+        return choice;
+    }
+
+    public static FunctionName GetRandomFunctionNameOtherThan(FunctionName name)
+    {
+        var choice = (FunctionName)Random.Range(1,function.Length);
+        if(choice == name) return 0;
+        else return choice;
+    }
+
+
 
     public static Vector3 Wave(float u, float v, float t)
     {
@@ -68,5 +95,19 @@ public static class FunctionLibrary
         p.y = r2 * Sin(PI * v);
         p.z = s * Cos(PI * u);
         return p;
+    }
+
+    public static Vector3 Special(float u,float v, float t)
+    {
+        Vector3 p;
+        p.x = Sin(PI * (u*(1f/17f) + v*(1f/19f) + t*(1f/23f)));
+        p.y = Sin(PI * (u*(1f/13f) + v*(1f/11f) + t*(1f/7f)));
+        p.z = Sin(PI * (u*(1f/2f) + v*(1f/3f) + t*(1f/5f)));
+        return p;
+    }
+
+    public static Vector3 Morph(float u, float v, float t, Function from, Function to, float progress)
+    {
+        return Vector3.LerpUnclamped(from(u,v,t),to(u,v,t), SmoothStep(0f,1f,progress));
     }
 }
